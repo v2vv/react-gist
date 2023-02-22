56b6c8f6d3e5611f837f3ac929f7885398c0d8f0
lvhongyuan <v2vvcn@gmail.com>
2023-02-22 21:37:17 +0800

    add marked package

--- src/App.js
+++ src/App.js
@@ -4,19 +4,33 @@ import { Octokit } from '@octokit/rest';
 
 export default function App() {
   useEffect(() => {
+
+  });
+
+  const [ vaule, ChangeVaule]  = useState("jj\r\nhh");
+
+  function handleClick(){
+    
     console.log('kk');
     const octokit = new Octokit({
       auth: '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',
     });
     octokit.gists.list().then(({ data }) => {
-      console.log(data);
+      // console.log(data);a
+      ChangeVaule(JSON.stringify(data,null,"\t"));
     });
-  });
-  const { vaule, ChangeVaule } = useState();
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello StackBlitz! </h1>
+      <button onClick={handleClick}>
+      You pressed me times
+    </button>
       <p>Start editing to see some magic happen :)</p>
+      <p>{vaule2}</p>
+      <p>{vaule}</p>
     </div>
+    
   );
 }
