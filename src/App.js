ce762225316d267a87411e7e444095f17fe029d5
lvhongyuan <v2vvcn@gmail.com>
2023-02-22 21:56:34 +0800

    解析gist list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 import { Octokit } from '@octokit/rest';
+import { marked } from 'marked';
 
 export default function App() {
   useEffect(() => {
 
   });
 
-  const [ vaule, ChangeVaule]  = useState("jj\r\nhh");
+  const [vaule, ChangeVaule] = useState("jj");
 
-  function handleClick(){
-    
+  function handleClick() {
+
     console.log('kk');
     const octokit = new Octokit({
       auth: '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',
     });
+
+    const filenames = [];
+
     octokit.gists.list().then(({ data }) => {
       // console.log(data);a
-      ChangeVaule(JSON.stringify(data,null,"\t"));
+      data.forEach((dat) => {
+        const files = dat.files
+        Object.keys(files).forEach((filename) => {
+          filenames.push(filename);
+        })
+
+      })
+      const html = marked.parse(JSON.stringify(filenames, null, "\r\n"));
+      ChangeVaule(html);
     });
   }
 
@@ -25,12 +37,11 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz! </h1>
       <button onClick={handleClick}>
-      You pressed me times
-    </button>
+        You pressed me times
+      </button>
       <p>Start editing to see some magic happen :)</p>
-      <p>{vaule2}</p>
-      <p>{vaule}</p>
+      <div dangerouslySetInnerHTML={{ __html: vaule }} />
     </div>
-    
+
   );
 }
