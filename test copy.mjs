af4547a1926edc3731418f43d472dac1a2839700
haitianlan <31479870+v2vv@users.noreply.github.com>
2023-03-30 05:52:58 +0000

    ChatGpt3.5 添加了中文注释后的代码

--- test copy.mjs
+++ test copy.mjs
@@ -8,10 +8,12 @@ const octokit = new Octokit({
 });
 
 async function displayGistData() {
+  //有 gist 数据 获取所
   const { data } = await octokit.request("GET /gists", {
     headers: { "X-GitHub-Api-Version": "2022-11-28" },
   });
 
+  // 并发请求每个 gist 的完整数据
   const responses = await Promise.all(
     data.map((dataq) =>
       octokit.request("GET /gists/{gist_id}", {
@@ -21,6 +23,7 @@ async function displayGistData() {
     )
   );
 
+  // 遍历每个 gist 的文件，并写入到磁盘中
   for (const response of responses) {
     const files = response.data.files;
     for (const filename in files) {
@@ -28,9 +31,11 @@ async function displayGistData() {
         const dir = "./gists";
         const content = files[filename].content;
         const path = `${dir}/${filename}`;
+        // 确保目录存在，如果不存在则创建它
         await fsPromises.mkdir(dir, { recursive: true });
+        // 将文件内容写入指定路径
         await fsPromises.writeFile(path, content);
-        console.log(`${path} written to file`);
+        console.log(`${path} 写入完成`);
       }
     }
   }
