4a5ab039a20cbbf673d6e5372647abcb028e33ac
v2vv <v2vvcn@gmail.com>
2023-04-01 17:52:10 +0000

    解决 eslint 与prettier冲突

--- src/App.js
+++ src/App.js
@@ -1,110 +1,112 @@
 /* eslint-disable camelcase */
-import { Octokit } from '@octokit/rest'
-import React, { useEffect, useState } from 'react'
-import './style.css'
-import hljs from 'highlight.js'
-import 'highlight.js/styles/obsidian.css'
-import { marked } from 'marked'
-import InputDialog from './page'
-import VConsole from 'vconsole'
+import { Octokit } from "@octokit/rest";
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import hljs from "highlight.js";
+import "highlight.js/styles/obsidian.css";
+import { marked } from "marked";
+import InputDialog from "./page";
+import VConsole from "vconsole";
 
 // eslint-disable-next-line no-new
-new VConsole()
+new VConsole();
 
-if (!localStorage.getItem('github_token')) { localStorage.setItem('github_token', 'vaule') }
+if (!localStorage.getItem("github_token")) {
+  localStorage.setItem("github_token", "vaule");
+}
 const sting1 = `
 echo hello
-`
+`;
 
-export default function App () {
+export default function App() {
   // refrash state
-  const [dialogVisible, setDialogVisible] = useState(false)
-  const [vaule, ChangeVaule] = useState('<p>jj</p')
-  const [html3, html3change] = useState('qqq')
-  const [context1, contex1change] = useState('eee')
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [vaule, ChangeVaule] = useState("<p>jj</p");
+  const [html3, html3change] = useState("qqq");
+  const [context1, contex1change] = useState("eee");
 
   // github token
-  const token_git = localStorage.getItem('github_token')
-  let redata = null
+  const token_git = localStorage.getItem("github_token");
+  let redata = null;
   const octokit = new Octokit({
-    auth: token_git
-  })
+    auth: token_git,
+  });
 
   // 输入 github token
   const handleOpenDialog = () => {
-    setDialogVisible(!dialogVisible)
-  }
+    setDialogVisible(!dialogVisible);
+  };
 
   // 内容输出触发函数
   const handleContextChange = async (key) => {
-    const contextTemp = await octokit.request('GET /gists/{gist_id}', {
-      gist_id: key
-    })
-    console.log(contextTemp.data)
+    const contextTemp = await octokit.request("GET /gists/{gist_id}", {
+      gist_id: key,
+    });
+    console.log(contextTemp.data);
     Object.keys(contextTemp.data.files).forEach((filenameTemp) => {
-      console.log(contextTemp.data.files[filenameTemp].content)
+      console.log(contextTemp.data.files[filenameTemp].content);
 
       marked.setOptions({
         highlight: function (code, lang) {
-          const language = hljs.getLanguage(lang) ? lang : 'auto'
-          return hljs.highlight(code, { language }).value
-        }
-      })
+          const language = hljs.getLanguage(lang) ? lang : "auto";
+          return hljs.highlight(code, { language }).value;
+        },
+      });
 
-      contex1change(marked.parse(contextTemp.data.files[filenameTemp].content))
-    })
-  }
+      contex1change(marked.parse(contextTemp.data.files[filenameTemp].content));
+    });
+  };
 
   useEffect(() => {
     // 配置 highlight.js
     hljs.configure({
       // 忽略未经转义的 HTML 字符
-      ignoreUnescapedHTML: true
-    })
+      ignoreUnescapedHTML: true,
+    });
     // 获取到内容中所有的code标签
-    const codes = document.querySelectorAll('pre code')
+    const codes = document.querySelectorAll("pre code");
     codes.forEach((el) => {
       // 让code进行高亮
-      hljs.highlightElement(el)
-    })
-  }, [])
+      hljs.highlightElement(el);
+    });
+  }, []);
 
-  async function handleClick () {
-    console.log('kk')
+  async function handleClick() {
+    console.log("kk");
 
     // 获取所有gists信息
-    redata = await octokit.request('GET /gists')
-    console.log(redata)
+    redata = await octokit.request("GET /gists");
+    console.log(redata);
     // 遍历 gists data
-    const filenames = {}
+    const filenames = {};
     redata.data.forEach((dat) => {
-      const files = dat.files
+      const files = dat.files;
       Object.keys(files).forEach((filename) => {
-        filenames[filename] = dat.id
-      })
-    })
-    console.log(filenames)
+        filenames[filename] = dat.id;
+      });
+    });
+    console.log(filenames);
     // 设置 hightlightjs json
     marked.setOptions({
       highlight: function (code, lang) {
-        const language = hljs.getLanguage(lang) ? lang : 'json'
-        return hljs.highlight(code, { language }).value
-      }
-    })
+        const language = hljs.getLanguage(lang) ? lang : "json";
+        return hljs.highlight(code, { language }).value;
+      },
+    });
     // 将 gists 信息输出到 markdown 格式字符串
     const html1 = marked.parse(
-      ' # gg \n```json\n' + JSON.stringify(filenames, null, '\n\t') + '\n```'
-    )
+      " # gg \n```json\n" + JSON.stringify(filenames, null, "\n\t") + "\n```"
+    );
 
     // 设置 hightlightjs bash
     marked.setOptions({
       highlight: function (code, lang) {
-        const language = hljs.getLanguage(lang) ? lang : 'bash'
-        return hljs.highlight(code, { language }).value
-      }
-    })
+        const language = hljs.getLanguage(lang) ? lang : "bash";
+        return hljs.highlight(code, { language }).value;
+      },
+    });
 
-    const html2 = marked.parse('\n```bash\n' + sting1 + '\n```')
+    const html2 = marked.parse("\n```bash\n" + sting1 + "\n```");
 
     html3change(
       <div className="context">
@@ -113,7 +115,7 @@ export default function App () {
             <li
               key={item}
               onClick={() => {
-                handleContextChange(filenames[item])
+                handleContextChange(filenames[item]);
               }}
             >
               {item}
@@ -121,10 +123,10 @@ export default function App () {
           ))}
         </ul>
       </div>
-    )
-    console.log(html3)
+    );
+    console.log(html3);
 
-    const html = html1 + html2
+    const html = html1 + html2;
     // const html = marked.parse(
     //   " # gg \n```json\n" + JSON.stringify(filenames, null, "\n") + "\n```"
     // );
@@ -132,8 +134,8 @@ export default function App () {
     // const Hhtml = hljs.highlightAuto(
     //   JSON.stringify(filenames, null, "\n")
     // ).value;
-    console.log(html)
-    ChangeVaule(html)
+    console.log(html);
+    ChangeVaule(html);
   }
 
   return (
@@ -151,5 +153,5 @@ export default function App () {
       <div id="flex_middle">{html3}</div>
       <div id="flex_right" dangerouslySetInnerHTML={{ __html: context1 }}></div>
     </div>
-  )
+  );
 }
